68301227278d3168d7739baf560e3b5043b5abaa
Lyudmil Pashayanov <sytix1@hotmail.com>
2018-10-05 11:35:13 +0200

    bridge

---
+++ Ink Knight/Assets/ButtonPress.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour {
+
+    public bool effectNr1 = false;
+    public GameObject bridge;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Interactables"))
+        {
+            Debug.Log("activate");
+            bridge.SetActive(true);
+        }
+    }
+}
