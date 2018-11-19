a2d7162ad6308468c9ebfdd226221c8a1a3ec93b
Lyudmil Pashayanov <sytix1@hotmail.com>
2018-11-19 22:01:48 +0100

    could build several bridges on different spots/ walk on bridge smoothly
    the material of the bridges should be fixed.

--- Ink Knight/Assets/BuildingBlocks Package/Scripts/ExportBuildedCube.cs
+++ Ink Knight/Assets/BuildingBlocks Package/Scripts/ExportBuildedCube.cs
@@ -3,43 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 public class ExportBuildedCube : MonoBehaviour {
-
-    public GameObject readyGameObject;
+     
+    //public GameObject readyGameObject;   legacy
     public Canvas mainGameCanvas;
     public Canvas buildingCanvas;
     public GameObject spawningPoint;
-   public GameObject prefabLoaded;
+   // public GameObject prefabLoaded;   legacy
     public GameObject buildingBlock;
     public GameObject builderScript;
-    //Creates a new menu (Examples) with a menu item (Create Prefab)
-    [MenuItem("Examples/Create Prefab")]
-    public void ExportAsPrefab()
+    public List<GameObject> activators;
+    public string builderActivatorName;
+    public GameObject blockPhase;
+    public GameObject sidePhase;
+    [MenuItem("Examples/Create Prefab")] // legacy, used to make a prefab of the builded thing.
+
+
+
+
+    //public void ExportAsPrefab()
+    //{
+    //    readyGameObject = GameObject.Find("BasicCube");
+    //    string localPath = "Assets/BuildingBlocks Package/Resources/" + readyGameObject.name + ".prefab";
+    //    Object prefab = PrefabUtility.CreateEmptyPrefab(localPath);
+    //    readyGameObject = PrefabUtility.CreatePrefab(localPath, readyGameObject, ReplacePrefabOptions.ReplaceNameBased);
+    //    mainGameCanvas.gameObject.SetActive(true);
+    //    buildingCanvas.gameObject.SetActive(false);
+    //    if (Resources.Load<GameObject>("BasicCube") == null)
+    //    {
+    //        Debug.Log("nullllllllllll");
+    //    }
+    //    prefabLoaded = Resources.Load<GameObject>("BasicCube");
+    //    prefabLoaded.GetComponent<Cube>().enabled = false;
+    //    prefabLoaded.GetComponent<AddCube>().enabled = false;
+    //    prefabLoaded.GetComponent<ChooseCubeToAdd>().enabled = false;
+    //    Instantiate(prefabLoaded, spawningPoint.transform.position, Quaternion.identity);
+    //}
+    public void BridgeReady()
     {
-        readyGameObject = GameObject.Find("BasicCube");
-        string localPath = "Assets/BuildingBlocks Package/Resources/" + readyGameObject.name + ".prefab";
-        Object prefab = PrefabUtility.CreateEmptyPrefab(localPath);
-        readyGameObject = PrefabUtility.CreatePrefab(localPath, readyGameObject, ReplacePrefabOptions.ReplaceNameBased);
         mainGameCanvas.gameObject.SetActive(true);
         buildingCanvas.gameObject.SetActive(false);
-        if (Resources.Load<GameObject>("BasicCube") == null)
-        {
-            Debug.Log("nullllllllllll");
-        }
-        prefabLoaded = Resources.Load<GameObject>("BasicCube");
-        prefabLoaded.GetComponent<Cube>().enabled = false;
-        prefabLoaded.GetComponent<AddCube>().enabled = false;
-        prefabLoaded.GetComponent<ChooseCubeToAdd>().enabled = false;
-        Instantiate(prefabLoaded, spawningPoint.transform.position, Quaternion.identity);
+        Destroy( GameObject.Find("BasicCube").GetComponent<Cube>());
+        Destroy(GameObject.Find("BasicCube").GetComponent<AddCube>());
+        Destroy( GameObject.Find("BasicCube").GetComponent<ChooseCubeToAdd> ());
+        blockPhase.gameObject.SetActive(true);
+        sidePhase.gameObject.SetActive(true);
+        GameObject.Find("BasicCube").AddComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        GameObject.Find("BasicCube").name = "bridge_" + builderActivatorName.ToString();
+        Destroy(GameObject.Find("selector"));
     }
-
+    public void assignSpawner()
+    {
+       spawningPoint = GameObject.Find("BuilderSpawningPoint_" + builderActivatorName);
+        Debug.Log(spawningPoint.name);
+    }
     public void startBuilding()
     {
         mainGameCanvas.gameObject.SetActive(false);
         buildingCanvas.gameObject.SetActive(true);
+        string name = EventSystem.current.currentSelectedGameObject.name;
         buildingBlock = Resources.Load<GameObject>("Builder");
         Instantiate(buildingBlock, spawningPoint.transform.position, Quaternion.identity);
+      
         builderScript.GetComponent<BuilderActivation>().getBuilder();
         builderScript.GetComponent<BuilderActivation>().AssignButtonInCanvas();
     }
+
 }
--- Ink Knight/Assets/Scripts/Lyudmil/ButtonPress.cs
+++ Ink Knight/Assets/Scripts/Lyudmil/ButtonPress.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class ButtonPress : MonoBehaviour {
     public GameObject bridge;
     public Button buildingButton;
+    public ExportBuildedCube expClass;
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Interactables"))
@@ -16,6 +18,15 @@ public class ButtonPress : MonoBehaviour {
         {
             Debug.Log("building button on");
             buildingButton.gameObject.SetActive(true);
+            foreach (GameObject item in expClass.activators)
+            {
+                if(gameObject.name == item.name)
+                {
+                    expClass.builderActivatorName = item.name;
+                    Debug.Log(expClass.builderActivatorName);
+                    expClass.assignSpawner();
+                }
+            }
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
