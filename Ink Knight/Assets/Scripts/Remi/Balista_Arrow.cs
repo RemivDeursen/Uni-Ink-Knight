7ca5678bda03880e3e444410a763cf630487759e
Remi van Deursen <cryslo1994@gmail.com>
2018-11-21 23:13:35 +0100

    Level improved and miniboss

---
+++ Ink Knight/Assets/Scripts/Remi/Balista_Arrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balista_Arrow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DestroyAfter5());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector2.right * Time.deltaTime * 10, Space.Self);
+	}
+
+	IEnumerator DestroyAfter5(){
+		yield return new WaitForSeconds(5);
+		Destroy(this.gameObject);
+	}
+}
---
+++ Ink Knight/Assets/Scripts/Remi/Balista_Button.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balista_Button : MonoBehaviour {
+	public enum Button_Type
+	{
+		down,
+		up,
+		fire
+	}
+	public Button_Type button_Type;
+	public Balista_Script balista;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+			Debug.Log("Hit Something");
+		if(other.gameObject.tag == "ButtonTrigger"){
+			Debug.Log("Hit player");
+			if(button_Type == Button_Type.down){
+				Debug.Log("Balista Down");
+				balista.MoveBalistaDOWN();
+			}
+			if(button_Type == Button_Type.up){
+				Debug.Log("Balista Up");
+				balista.MoveBalistaUP();
+			}
+			if(button_Type == Button_Type.fire){
+				balista.FireBalista();
+			}
+		}
+	}
+}
